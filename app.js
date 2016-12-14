066c1254c00d8b0269643d9fdce5bfe5633aadbd
Abhinavvisen <visenabhinav96@gmail.com>
2016-12-14 23:51:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,3 +51,17 @@ var fs = require('fs');
 fs.unlink('./stuff/writeMe.txt',function(){
   fs.rmdir('stuff');
 });
+
+
+
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+  console.log('request was made'+req.url);
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('hey ninjas');
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('yo dawgs now listening');
+
