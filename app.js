38bfda7f1c51352dcdab28067d1628ae90b4b9d8
Abhinavvisen <visenabhinav96@gmail.com>
2016-12-15 21:02:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -122,3 +122,27 @@ myReadStream.on('data',function(chunk){
   console.log('new chunk Received');
   myWriteStream.write(chunk);
 });
+
+
+
+
+
+
+
+
+
+var http = require('http');
+
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+  console.log('request was made'+req.url);
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  var myReadStream = fs.createReadStream(__dirname+'/readMe.txt','utf8');
+
+  myReadStream.pipe(res);
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('yo dawgs now listening');
+
