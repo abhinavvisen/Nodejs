37c0d968717391cd143a948ae70db7c18ae20980
Abhinavvisen <visenabhinav96@gmail.com>
2016-12-15 20:20:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -65,3 +65,49 @@ var server = http.createServer(function(req,res){
 server.listen(3000,'127.0.0.1');
 console.log('yo dawgs now listening');
 
+
+
+
+
+
+
+
+var http = require('http');
+
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname+'/readMe.txt','utf8');
+
+myReadStream.on('data',function(chunk){
+  console.log('new chunk Received');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var server = http.createServer(function(req,res){
+  console.log('request was made'+req.url);
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('hey ninjas');
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('yo dawgs now listening');*/
+
