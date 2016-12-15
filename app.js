77251fdabe4f47225bcc6cfcf9d2b0a3969caaef
Abhinavvisen <visenabhinav96@gmail.com>
2016-12-16 03:33:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -179,3 +179,23 @@ fs.createReadStream(__dirname+'/index.html').pipe(res);
 server.listen(3000,'127.0.0.1');
 console.log('yo dawgs now listening');
 
+
+
+
+var express = require('express');
+
+var app = express();
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/contact',function(req,res){
+  res.sendFile(__dirname+'/contact.html');
+});
+
+app.get('/profile/:id',function(req,res){
+  res.send('you lookong with profile id of '+req.params.id);
+});
+app.listen(3000);
+
