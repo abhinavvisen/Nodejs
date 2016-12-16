9a6af31e615994e48e6079aa71f85bfd66a7e69b
Abhinavvisen <visenabhinav96@gmail.com>
2016-12-16 13:21:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -199,3 +199,29 @@ app.get('/profile/:id',function(req,res){
 });
 app.listen(3000);
 
+
+
+
+
+
+var express = require('express');
+
+var app = express();
+
+app.set('view engine','ejs');
+
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/contact',function(req,res){
+  res.sendFile(__dirname+'/contact.html');
+});
+
+app.get('/profile/:name',function(req,res){
+  var data = {Age:29,job:'ninja',hobbies:['eating','fighting','fishing']};
+  res.render('profile',{person:req.params.name,data:data});
+});
+app.listen(3000);
+
